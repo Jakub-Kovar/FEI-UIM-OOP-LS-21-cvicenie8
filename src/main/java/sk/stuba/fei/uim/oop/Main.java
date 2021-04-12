2d2c9f7e8a9f95d21f5f256198b36d1d83ccc01b
Jakub Kovář <jakub.kovar@stuba.sk>
2021-04-12 13:22:21 +0200

    labeled break

--- src/main/java/sk/stuba/fei/uim/oop/Main.java
+++ src/main/java/sk/stuba/fei/uim/oop/Main.java
@@ -2,6 +2,16 @@ package sk.stuba.fei.uim.oop;
 
 public class Main {
     public static void main(String[] args) {
-
+        vonkajsiFor:
+        for (int i = 1; i < 100; i++) {
+            for (int j = 10; j > 0; j--) {
+                System.out.println("---");
+                if (i%4 == 0) {
+                    break vonkajsiFor;
+                }
+                System.out.println(i + ", " + j);
+            }
+        }
+        System.out.println("koniec");
     }
 }
